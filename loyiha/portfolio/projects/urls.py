a0ac8c3642779e17988e00ba1e8446be74370662
Oybek 1 <Oybeklinux@gmail.com>
2022-09-09 17:00:57 +0500

    lesson8

--- loyiha/portfolio/projects/urls.py
+++ loyiha/portfolio/projects/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('projects/', projects),
-    path('projects/<int:pk>', project)
+    path('projects/<int:pk>', project, name='project'),
+    path('add_project/', add_project, name="add_project"),
+
 ]
--- loyiha/portfolio/projects/views.py
+++ loyiha/portfolio/projects/views.py
@@ -2,13 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 # Create your views here.
-
+from .forms import ProjectForm
 
 def projects(request):
-    items = Project.objects.all().values()
-    return render(request, "projects/projects.html")
+    items = Project.objects.all()
+    return render(request, "projects/projects.html", {"items": items})
 
 
 def project(request, pk):
     item = Project.objects.get(id=pk)
-    return HttpResponse(item.__dict__.items())
+    return render(request, "projects/project.html", {"item": item})
+
+
+def add_project(request):
+    form = ProjectForm()
+    return render(request, "projects/add_project.html", {"form": form})
